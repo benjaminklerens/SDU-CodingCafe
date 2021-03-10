e06b01497a6580b0be97b0b826a179113e0cf07e
Benjamin Klerens <bekle18@student.sdu.dk>
2021-03-10 15:27:18 +0100

    Initial commit

---
+++ request.js
@@ -0,0 +1,43 @@
+const http = require('http')
+
+const options_root = {
+    host: 'localhost',
+    port: 8000,
+    path: '/',
+    method: 'GET',
+    headers: {}
+}
+
+const options_benjamin = {
+    host: 'localhost',
+    port: 8000,
+    path: '/benjamin',
+    method: 'GET',
+    headers: {}
+}
+
+const options_sarah = {
+    host: 'localhost',
+    port: 8000,
+    path: '/sarah',
+    method: 'GET',
+    headers: {}
+}
+
+
+callback = function(response) {
+    var str = ''
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+  
+    response.on('end', function () {
+      console.log(str);
+    });
+  }
+  
+  var req = http.request(options_benjamin, callback);
+  req.write('')
+  req.end();
+
+
---
+++ script.js
@@ -0,0 +1,33 @@
+// Require express and create an instance of it
+var express = require('express');
+var app = express();
+
+// on the request to root (localhost:3000/)
+app.get('/', function (req, res) {
+    res.send("Velkommen til Code Café's root");
+});
+
+app.get('/benjamin', function (req, res) {
+    res.json({
+        firstname: "Benjamin",
+        lastname: "Klerens",
+        age: 24,
+        fieldofstudy: "Software Engineering"
+    })
+});
+
+app.get('/sarah', function (req, res) {
+    res.send("Velkommen til Code Café's root");
+});
+
+
+
+// Change the 404 message modifing the middleware
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
+});
+
+// start the server in the port 8000 !
+app.listen(8000, function () {
+    console.log('Example app listening on port 8000.');
+});
