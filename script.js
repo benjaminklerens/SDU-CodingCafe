f289c2913128e199db63df760c6b74dff17afa07
Benjamin Klerens <bekle18@student.sdu.dk>
2021-03-10 15:47:03 +0100

    Heroku fixing

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var app = express();
 
+var server_port = process.env.PORT || 8000;
+
 // on the request to root (localhost:3000/)
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -24,10 +26,10 @@ app.get('/sarah', function (req, res) {
 
 // Change the 404 message modifing the middleware
 app.use(function(req, res, next) {
-    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
+    res.status(404).send("Ruten eksisterer desværre ikke :(");
 });
 
 // start the server in the port 8000 !
-app.listen(8000, function () {
-    console.log('Example app listening on port 8000.');
+app.listen(server_port, function () {
+    console.log('Server åbnet på port 8000.');
 });
